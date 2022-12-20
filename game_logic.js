1277e2b18510c11ac6672ffa0d50ae2a3e2ab4b7
Max Burrows <maxajburrows@gmail.com>
2022-12-20 18:23:45 +0100

    Write game function

--- game_logic.js
+++ game_logic.js
@@ -9,3 +9,28 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
+
+function playRound(playSelect, compSelect) {
+    playSelect = capitalise(playSelect)
+    if (!(playSelect=="Rock" || playSelect=="Scissors" || playSelect=="Paper")) {
+        return "Oops it looks like you made a spelling error!"
+    }
+    if (playSelect==compSelect) {
+        return `Draw! You both picked ${playSelect}`
+    } else if ((playSelect=="Rock" && compSelect=="Scissors")||(playSelect=="Scissors" && compSelect=="Paper")||(playSelect=="Paper" && compSelect=="Rock")) {
+        return `You Win! ${playSelect} beats ${compSelect}`
+    } else {
+        return `You Lose! ${compSelect} beats ${playSelect}`
+    }
+}
+
+function capitalise(word) {
+    let wordLowerCase = word.toLowerCase();
+    let firstLetterLower = wordLowerCase.slice(0,1);
+    let firstLetterUpper = firstLetterLower.toUpperCase();
+    return wordLowerCase.replace(firstLetterLower, firstLetterUpper);
+}
+
+const playerSelection = "PAPER";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

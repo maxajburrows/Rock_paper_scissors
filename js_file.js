8e4a340ab96d48d854b9d8c1cae930f04f32bc3f
Max Burrows <maxajburrows@gmail.com>
2023-01-19 12:51:02 +0100

    Add score keeping functionality and message

--- js_file.js
+++ js_file.js
@@ -12,27 +12,48 @@ function getComputerChoice() {
 
 function playRound(e) {
     let compSelect = getComputerChoice();
-    //let playSelect = e.path<0<id;
     let playSelect = this.id;
     let resultMessage = document.querySelector('#message');
+    let result
      
     if (playSelect==compSelect) {
         resultMessage.textContent = `Draw! You both picked ${playSelect}.`;
-        return 'Draw'
+        result = 'Draw';
     } else if ((playSelect=="Rock" && compSelect=="Scissors")||(playSelect=="Scissors" && compSelect=="Paper")||(playSelect=="Paper" && compSelect=="Rock")) {
         resultMessage.textContent = `You Win! ${playSelect} beats ${compSelect}.`;
-        return 'Player Win'
+        result = 'Player Win';
     } else {
         resultMessage.textContent = `You Lose! ${compSelect} beats ${playSelect}.`;
-        return 'Computer Win'
+        result = 'Computer Win';
     }
+    score(result, firstTo, resultMessage);
 }
 
-function capitalise(word) {
-    let wordLowerCase = word.toLowerCase();
-    let firstLetterLower = wordLowerCase.slice(0,1);
-    let firstLetterUpper = firstLetterLower.toUpperCase();
-    return wordLowerCase.replace(firstLetterLower, firstLetterUpper);
+const firstTo = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+function score(result, firstTo, resultMessage) {
+    const scoreMessage = document.querySelector('#score');
+    console.log(firstTo);
+    console.log(playerScore);
+
+    if (playerScore < firstTo && computerScore < firstTo) {
+        if (result=='Player Win') {
+            playerScore++;
+        } else if (result=='Computer Win') {
+            computerScore++;
+        }
+        scoreMessage.textContent = `The score is: You ${playerScore} - ${computerScore} The Machines`;
+    }
+    if (playerScore === firstTo) {
+        resultMessage.textContent = 'You win! Humans survive...for now.';
+        scoreMessage.textContent = `Final score: You ${playerScore} - ${computerScore} The Machines`;
+
+    } else if (computerScore === firstTo) {
+        resultMessage.textContent = 'You Lose! The machines are taking over';
+        scoreMessage.textContent = `Final score: You ${playerScore} - ${computerScore} The Machines`;
+    }
 }
 
 function game() {
@@ -62,6 +83,3 @@ function game() {
 const btns = document.querySelectorAll('button');
 console.log(btns);
 btns.forEach(btn => btn.addEventListener('click', playRound));
-
-
-//console.log(game());
